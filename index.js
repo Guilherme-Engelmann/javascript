73538c828f8df78bf8077f94d59e398b27ed395d
Guilherme-Engelmann <guilherme_engelmann@estudante.sesisenai.org.br>
2025-03-20 10:00:28 -0300

    add

--- index.js
+++ index.js
@@ -1,15 +1,50 @@
-console.log('Hello World'); 
+/*console.log('Verificador de par'); 
 let numero = prompt('Digite seu número:');
 function verificarPar(numero){
 
     if(numero % 2 === 0){
-        console.log(numero + "é igual a par");
+        console.log(numero + " é igual a par");
 
     }else{
-        console.log(numero + "é igual a impar");
+        console.log(numero + " é igual a impar");
 
     }
+        
 
 }
 
-verificarPar(numero);
+
+verificarPar(numero);*/
+
+/*
+console.log('Somador');
+let numero1 = parseFloat(prompt('Digite seu número'));
+let numero2 = parseFloat(prompt('Digite seu número'));
+function calcular(numero1,numero2){
+   const soma = numero1 + numero2;
+   const subtracao = numero1 - numero2;
+   const multiplicacao = numero1 * numero2;
+   console.log(soma);
+   console.log(subtracao);
+   console.log(multiplicacao);
+   if(numero2 === 0){
+    console.log("Não da pra dividir por 0!");
+   }else{
+    const divisao = numero1 / numero2;
+    console.log(divisao);
+   }
+    
+
+
+}
+
+calcular(numero1,numero2);8 */
+
+console.log('Contadora');
+let i = 0;
+
+ while(i< 1){
+    console.log('1 X ' + i + '=' + 1*i);
+ }
+
+
