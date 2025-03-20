4325a202a32062074e6d02cf289732d49b70334b
Guilherme-Engelmann <guilherme_engelmann@estudante.sesisenai.org.br>
2025-03-20 09:08:56 -0300

    add

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 console.log('Hello World'); 
-
+let numero = prompt('Digite seu número:');
 function verificarPar(numero){
-
-    let numero = prompt('Digite seu número:');
 
     if(numero % 2 === 0){
         console.log(numero + "é igual a par");
 
     }else{
-        console.log(numero + "é igual a par");
+        console.log(numero + "é igual a impar");
 
     }
 
 }
+
+verificarPar(numero);
