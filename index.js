0c65783cc550e2b017a868751cc6ddec0490b76a
Guilherme-Engelmann <guilherme_engelmann@estudante.sesisenai.org.br>
2025-03-21 10:02:04 -0300

    add

--- index.js
+++ index.js
@@ -40,15 +40,41 @@ function calcular(numero1,numero2){
 
 calcular(numero1,numero2);8 */
 
+/*
 console.log('Contadora');
 
-let Contadora = 1;
+let contadora = 1;
 
-const valormaximo = 10;
- while(contadora <= valormaximo){
+ while(contadora <= 10){
     console.log(contadora);
     contadora++;
- }
-    
+ }*/
+/*
+console.log('Strings')
+
+let texto = prompt('Digite seu texto');
+function invertertexto(texto){
+   console.log(texto.split("").reverse().join(""));
+}
+invertertexto(texto);*/
+
+/*
+console.log('Contar caracteres');
+let texto = prompt("Digite o texto");
 
 
+contarcaracteres(texto)
+
+function contarcaracteres(texto){
+    console.log(texto.length);
+}*/
+/*
+console.log('Propriedades do objeto: Carro');
+let marca = prompt("Digite a marca do carro");
+let modelo = prompt("Digite o modelo do carro");
+let ano = prompt("Digite o ano do carro");
+console.log(marca);
+console.log(modelo);
+console.log(ano);*/
+
+  
